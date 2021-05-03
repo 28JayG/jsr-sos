1637b0d94f9ee79add1594409b806baa16222b7e
28JayG <jaygandhi.32@outlook.com>
2021-05-03 13:15:03 +0530

    - updates servies iconUrl - renames icon to iconUrl in services

--- src/components/homepage/services-overview-section/service-tile/service-tile.component.jsx
+++ src/components/homepage/services-overview-section/service-tile/service-tile.component.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 import './service-tile.styles.scss';
 
-const ServiceTile = ({ icon, title, route }) => {
+const ServiceTile = ({ iconUrl, title, route }) => {
   return (
     <Link to={`${route}`} className="service-tile">
       {/*icon */}
-      <div className="icon" style={{ backgroundImage: `url(${icon})` }} />
+      <div className="icon" style={{ backgroundImage: `url(${iconUrl})` }} />
       <h4 className="title">{title}</h4>
     </Link>
   );
